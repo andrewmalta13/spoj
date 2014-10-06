ce77da278f115f28706fc4d19b52f241eeab57e3
Andrew Malta <andrew.malta@yale.edu>
2014-10-06 16:25:34 -0400

    fixed some pagination issues.

--- addrev/addrev.cpp
+++ addrev/addrev.cpp
@@ -2,29 +2,29 @@
 using namespace std;
 
 int reverseNumber(int num){
-	int reversed = 0;
-	while(num){  // we reached the end of the digits
+    int reversed = 0;
+    while(num){  // we reached the end of the digits
         reversed += (num % 10); //add to the ones place
         num /= 10;     //shift to the left
 
         if(num) reversed *= 10; // shift to the right, but avoid adding an extra
                                 // zero at the end.
-	}
+    }
     return reversed;
 }
 
 int main(){
-	int numTestCases;
-	cin >> numTestCases;
-	int a, b;
-	int sum;
+    int numTestCases;
+    cin >> numTestCases;
+    int a, b;
+    int sum;
     for(int i=0; i<numTestCases; i++){
-    	cin >> a >> b;
-    	sum = reverseNumber(a) + reverseNumber(b);
-    	cout << reverseNumber(sum) << "\n";
+        cin >> a >> b;
+        sum = reverseNumber(a) + reverseNumber(b);
+        cout << reverseNumber(sum) << "\n";
     }
 
-	
-	return 0;
+
+    return 0;
 }
 
